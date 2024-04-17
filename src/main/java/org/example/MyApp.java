722280aac2ccb62f06a1d26b2418c0b61a5add25
theclouday <theclouday@example.com>
2024-04-17 12:41:10 +0200

    testing output

--- src/main/java/org/example/MyApp.java
+++ src/main/java/org/example/MyApp.java
@@ -5,19 +5,36 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 public class MyApp
 {
     private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static String[] args;
+
+    public static void setArgs(String[] args) {
+        MyApp.args = args;
+    }
+
+    public static String[] getArgs() {
+        return args;
+    }
+
     {
         objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     }
-    public static void main(String[] args) throws IOException {
-        GettingAllFiles wwl = new GettingAllFiles("D:\\Java Projects\\tesate\\statistik\\src\\main\\resources");
 
-        CollectionOfStatistics collection = new CollectionOfStatistics(2);
+    public static void main(String[] args) throws IOException {
+
+
+        String[] path = new String[]{"D:\\Java Projects\\tesate\\statistik\\src\\main\\resources"};
+        setArgs(path);
+
+        Arrays.asList(args).forEach(System.out::println);
+        
+        CollectionOfStatistics collection = new CollectionOfStatistics(5);
 
         List<BookStatistics> result = collection.makeStatistics();
 
