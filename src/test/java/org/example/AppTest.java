d87849ae917108c8d26b948380cb592d0c84302d
theclouday <theclouday@example.com>
2024-04-17 12:42:20 +0200

    some commit

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -6,14 +6,5 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ScriptAppTest {
 
-    @Test
-    void inputVoidPath()
-    {
-        GettingAllFiles getAllFiles = new GettingAllFiles("D:\\Java Projects\\tesate\\statistik\\src\\main\\resources");
-        String userPath = GettingAllFiles.getPath();
-        assertNotNull(userPath, "Uncorrected Path");
-
-    }
-
 
 }
