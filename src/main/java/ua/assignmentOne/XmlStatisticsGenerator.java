cdbf13ad000f374cc5ec59aabeebecf41b6095d9
theclouday <theclouday@example.com>
2024-04-18 23:11:55 +0200

    has standardized the code

--- src/main/java/ua/assignmentOne/XmlStatisticsGenerator.java
+++ src/main/java/ua/assignmentOne/XmlStatisticsGenerator.java
@@ -9,14 +9,16 @@ import ua.assignmentOne.service.BookStatisticsService;
  */
 
 public class XmlStatisticsGenerator {
+
     /**
      * The entry point to the application for generating statistics in XML format.
      *
      * @param args Command line arguments. Two arguments are expected to be passed: the path to the directory with JSON files and the name of the field for statistics aggregation.
      */
+
     public static void main(String[] args) {
 
-        BookStatisticsService bookStatisticsService = new BookStatisticsService(5);
+        BookStatisticsService bookStatisticsService = new BookStatisticsService();
         bookStatisticsService.generateStatisticsFromFiles(args[0], args[1]);
 
     }
