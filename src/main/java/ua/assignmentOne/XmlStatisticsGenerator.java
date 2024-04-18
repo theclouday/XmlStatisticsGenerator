cb8074955db01d4cd355051539bd446a17c93ae7
theclouday <theclouday@example.com>
2024-04-18 22:48:59 +0200

    formating

--- src/main/java/ua/assignmentOne/XmlStatisticsGenerator.java
+++ src/main/java/ua/assignmentOne/XmlStatisticsGenerator.java
@@ -1,22 +1,23 @@
 package ua.assignmentOne;
 
-import ua.assignmentOne.service.BookService;
+import ua.assignmentOne.service.BookStatisticsService;
 
 /**
  * Generator of statistics in XML format based on data from JSON files.
+ *
  * @author Maksym Filonov
  */
 
-public class XmlStatisticsGenerator
-{
+public class XmlStatisticsGenerator {
     /**
      * The entry point to the application for generating statistics in XML format.
+     *
      * @param args Command line arguments. Two arguments are expected to be passed: the path to the directory with JSON files and the name of the field for statistics aggregation.
      */
     public static void main(String[] args) {
 
-        BookService bookService = new BookService(5);
-        bookService.printStatToXmlFile(args[0], args[1]);
+        BookStatisticsService bookStatisticsService = new BookStatisticsService(5);
+        bookStatisticsService.generateStatisticsFromFiles(args[0], args[1]);
 
     }
 }
